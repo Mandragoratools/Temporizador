cdd2b747389aa010cbad2f2e3f40f6455f72f204
Mandragora Tools <mandragoratools@gmail.com>
2016-12-23 01:29:21 +0100

    Update Temporizador_bucle.ino

--- Ejemplos/Temporizador_bucle/Temporizador_bucle.ino
+++ Ejemplos/Temporizador_bucle/Temporizador_bucle.ino
@@ -15,7 +15,7 @@ void setup() {
 
 pinMode(2,OUTPUT); //-----> Declaramos el pin 2 como salida
 
-temp.iniciar(0,0,0,1,0);//-----> Iniciamos el temporizador por primera ves en este caso a un segundo
+temp.temporizar(0,0,0,1,0);//-----> Iniciamos el temporizador por primera ves en este caso a un segundo
 
 }
 
@@ -24,7 +24,7 @@ void loop() {
 //comprobamos si el tiempo definido ha transcurrido
 if(temp.completado()){ 
   digitalWrite(2,!digitalRead(2)); //-----> En este caso al transcurrir el tiempo indicado invertimos el estado del pin 2
-  timer1.iniciar(0,0,0,1,0); //-----> volvemos a iniciar el temporizador para que se repita de nuevo en modo bucle
+  timer1.temporizar(0,0,0,1,0); //-----> volvemos a iniciar el temporizador para que se repita de nuevo en modo bucle
 }
 
 
